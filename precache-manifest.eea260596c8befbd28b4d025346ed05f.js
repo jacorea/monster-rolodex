1b548a60781624658473eceea8ab9505ae87abe1
jcorea <jacorea410@gmail.com>
2019-09-05 17:36:54 -0700

    Updates

---
+++ precache-manifest.eea260596c8befbd28b4d025346ed05f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe773c8f7a50cbe6c53a5e0a0fe54b8c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3e854d980b9dadea1d19",
+    "url": "/monster-rolodex/static/css/main.00656629.chunk.css"
+  },
+  {
+    "revision": "9030302ccbfe955266e0",
+    "url": "/monster-rolodex/static/js/2.11aa2f1f.chunk.js"
+  },
+  {
+    "revision": "3e854d980b9dadea1d19",
+    "url": "/monster-rolodex/static/js/main.38e3d69c.chunk.js"
+  },
+  {
+    "revision": "4d8b09cc10078b9bdb7b",
+    "url": "/monster-rolodex/static/js/runtime~main.aa7a915c.js"
+  }
+]);
